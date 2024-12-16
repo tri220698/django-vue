0d54c5910ca26ffef0665617ad51c0981cae0863
trimn-1012 <mai.ngoc.tri@sun-asterisk.com>
2024-12-16 16:29:45 +0700

    initial source code fe

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <p class="text-neutral-900">Hello</p>
+  </header>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+
+</style>
